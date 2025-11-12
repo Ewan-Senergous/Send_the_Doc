c09f850eec70ced7913d904ae1a939e6109c1b4c
Ewan-Senergous <ewan16270409@outlook.fr>
2025-11-12 15:28:44 +0100

    "recycle" add woocomerce plugins

--- Word-Press/vendor/composer/autoload_static.php
+++ Word-Press/vendor/composer/autoload_static.php
@@ -6,6 +6,20 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit249a9d268e2f167b738158acddd9b130
 {
+    public static $prefixLengthsPsr4 = array (
+        'C' => 
+        array (
+            'Composer\\Installers\\' => 20,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Composer\\Installers\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
+        ),
+    );
+
     public static $prefixesPsr0 = array (
         'j' => 
         array (
@@ -23,6 +37,8 @@ class ComposerStaticInit249a9d268e2f167b738158acddd9b130
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit249a9d268e2f167b738158acddd9b130::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit249a9d268e2f167b738158acddd9b130::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit249a9d268e2f167b738158acddd9b130::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit249a9d268e2f167b738158acddd9b130::$classMap;
 
